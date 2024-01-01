cb186a801ace4c5f9f3d3eb58865602cde0247c0
PranayBisan45 <pranaybisan45@gmail.com>
2024-01-01 14:39:15 +0530

    new

---
+++ Authentication/Authentication/BOL/Login.cs
@@ -0,0 +1,6 @@
+namespace BOL;
+public class Login{
+    public int id{get;set;}
+    public String name{get;set;}
+    public String password{get;set;}
+}
---
+++ Authentication/Authentication/BOL/Register.cs
@@ -0,0 +1,6 @@
+namespace BOL;
+public class Registration {
+    public int id { get; set; }
+    public String name { get; set; }
+    public String password { get; set; }
+}
